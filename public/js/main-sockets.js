7cc4d312a68eeafe1ba3df3c4307563128845def
Raúl Sampedro <rsrdesarrollo@gmail.com>
2015-06-04 20:51:17 +0200

    add bootstrap,jquery. Sample job. Socket.io comm definition

---
+++ public/js/main-sockets.js
@@ -0,0 +1,51 @@
+$(function(){
+    var socket = io();
+
+    var message_box = $("#message");
+    var progress_bar = $("#job-progress");
+    var message_txt = $("#message-txt");
+    var url_query = $("#url_query");
+
+    function update_progress_bar(value){
+        var pb = progress_bar.children(".progress-bar");
+        pb.attr("aria-valuenow", value);
+        pb.width(value+"%");
+        
+    }
+
+    function submit_job(event){
+        event.preventDefault();
+        socket.emit('new job', {url_query:url_query.val()});
+    }
+
+    function notify_job_start(data){
+        message_box.slideDown();
+        message_box.addClass('alert-info');
+        message_box.removeClass('alert-success');
+        message_txt.text("Job submited success. Starting job...");
+        progress_bar.show();
+        
+        update_progress_bar(0);
+    }
+
+    function notify_job_status(data){
+        message_txt.text("Runing Job ..."+data.done+"/"+data.total);
+        update_progress_bar(data.progress);
+    }
+
+    function notify_job_end(data){
+        message_box.removeClass('alert-info');
+        message_box.addClass('alert-success');
+        progress_bar.fadeOut();
+        message_txt.html("<span>Job finished: </span><a href='"+data.file_result+"'> Download result</a>");
+
+    }
+
+    socket.on('job started', notify_job_start);
+    socket.on('job status', notify_job_status);
+    socket.on('job end', notify_job_end);
+
+    $("#job").submit(submit_job);
+
+});
+
--- sockets.js
+++ sockets.js
@@ -1,3 +1,39 @@
+function demo_sample_job(client){
+    var aux = 0;
+    var progress = function(){
+        aux += 10;
+        if(aux < 100){
+            setTimeout(progress, 100);
+            client.emit('job status', {
+                done: aux,
+                total: 100,
+                progress: aux
+            }); 
+        }else{
+            client.emit('job end', {file_result:"/js/jquery.js"});
+        }
+        
+
+        console.log("Job progress:", aux);
+
+    };
+
+    progress();
+}
+
+function handle_new_job(client, data){
+    
+    console.log("New Job", data, client);
+    client.emit('job started');
+
+    demo_sample_job(client);
+}
+
 module.exports = function(io){
-	
+    io.on('connect', function(socket){
+        socket.on('new job', handle_new_job.bind(null,socket));
+        
+        socket.on('disconnect', function(){
+        });
+    });
 }
