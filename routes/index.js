06e6b55a09e4a5891a8fa2fecd5d469b6fa3159c
Raúl Sampedro <rsrdesarrollo@gmail.com>
2015-06-07 15:34:06 +0200

    change project name

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Proxy NAV Exporter' });
+  res.render('index', { title: 'Kibana CSV Exporter' });
 });
 
 module.exports = router;
